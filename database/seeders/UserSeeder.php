4053797c59f9410271265086c6dfb924b649e0d5
Maximilian Mewes <mewes.maximilian@gmx.de>
2023-03-27 15:18:41 +0200

    Add `UserFactory` and `UserSeeder`

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        // always create one root user
+        User::create([
+            'name' => 'Admin',
+            'firstname' => 'Blubber',
+            'lastname' => 'Lounge',
+            'email' => 'admin@blubber-lounge.de',
+            'telefon_mobil' => '+49 152 33869318',
+            'dob' => date('Y-m-d H:i:s', strtotime('02.11.1999')),
+            'email_verified_at' => now(),
+            'password' => Hash::make('blem-r00t'),
+        ]);
+
+        User::factory()
+            ->count(rand(10, 18))
+            ->create();
+    }
+}
