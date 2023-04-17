799119ba7d93b01ca2f8ebc70f702a03684bb5f2
Maximilian Mewes <mewes.maximilian@gmx.de>
2023-04-17 20:37:25 +0200

    Add Fancy Topnavbar transitions in settings

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,39 @@
-import './bootstrap';
-import Glide from '@glidejs/glide';
+import "./bootstrap";
+import Glide from "@glidejs/glide";
 
-var glide = new Glide('#glide', {
-    type: 'slider',
+var glide = new Glide("#glide", {
+    type: "slider",
     startAt: 0,
     perView: 1,
-    focusAt: 'center',
+    focusAt: "center",
     touchAngle: 30,
     swipeThreshold: 40,
     breakpoints: {
-      800: {
-        perView: 2
-      },
-      480: {
-        perView: 1
-      }
-    }
-  })
+        800: {
+            perView: 2,
+        },
+        480: {
+            perView: 1,
+        },
+    },
+});
 
-  glide.mount()
+glide.mount();
+
+const animateCSS = (element, animation, prefix = "animate__") =>
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = document.querySelector(element);
+
+        node.classList.add(`${prefix}animated`, animationName);
+
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve("Animation ended");
+        }
+
+        node.addEventListener("animationend", handleAnimationEnd, {
+            once: true,
+        });
+    });
--- resources/views/includes/top-nav.blade.php
+++ resources/views/includes/top-nav.blade.php
@@ -1,49 +1,52 @@
 <nav id="topNav" class="navbar">
     <div class="container" style="max-width: 1200px">
-        <div class="row w-100 justify-content-between align-items-center">
-            <div class="col">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="https://media.maximilian-mewes.de/project/bl/blubber_lounge_rebrand_try_white.svg" id="navBrand" alt="Dart a Web-App Logo" width="75px">
-                </a>
-            </div>
-            @if(request()->routeIs('settings'))
-                <div class="col-1 d-flex justify-content-center">
-                    <a href="#">
-                        <i class="fa-solid fa-pencil"></i>
-                    </a>
-                </div>
-            @endif
-            <div class="col-2 d-flex justify-content-center">
-                <a href="#" class="d-flex align-items-center">
-                    <i class="fa-solid fa-qrcode"></i>
-                </a>
-            </div>
-            <div class="col-1 d-flex justify-content-center" onclick="$('#burger-menu').toggle()">
-                <div class="burger-menu-toggler" style="cursor: pointer;padding:.1rem .5rem;">
-                    <div class="burger"></div>
-                    <div class="burger"></div>
-                    <div class="burger"></div>
-                </div>
-            </div>
+        <div @class([ 'col-auto' => request()->routeIs('settings'), 'col' => !request()->routeIs('settings') ])>
+            <a class="navbar-brand m-0" href="{{ url('/') }}">
+                <img src="https://media.maximilian-mewes.de/project/bl/blubber_lounge_rebrand_try_white.svg" id="navBrand" alt="Dart a Web-App Logo" width="75px">
+            </a>
         </div>
         @if(request()->routeIs('settings'))
-            <div class="row pt-4 pb-1">
-                <div style="flex: 0 0 auto;max-width: 70px;">
+            <div id="moveMe" class="col d-flex align-items-center ps-2" style="font-size:.75rem;">
+                <div class="me-2">
                     @if(Auth::user()->img)
-                        <img src="{{ Auth::user()->img }}" width="50px" style="border-radius:50%">
+                        <img src="{{ Auth::user()->img }}" width="30px" style="border-radius:50%">
                     @else
                         {!! Avatar::create(Auth::user()->name)->setDimension(50)->setFontSize(28)->toSvg() !!} {{-- https://github.com/laravolt/avatar --}}
                     @endif
                 </div>
-                <div class="col">
-                    <span class="h4"> {{ Auth::user()->firstname .' '. Auth::user()->lastname }} </span>
+                <div style="line-height: 1">
+                    <span class="h4 m-0"> {{ Auth::user()->firstname .' '. Auth::user()->lastname }} </span>
                     <span class="d-block m-0 p-0" style="color: var(--clr-gray-50);"> {{ '@'.Auth::user()->name }} </span>
                 </div>
             </div>
-            <div class="update-profileimage">
-                <i class="fa-solid fa-camera"></i>
+            <div class="col-1 d-flex justify-content-center ">
+                <a href="#" class="d-flex align-items-center">
+                    <i class="fa-solid fa-qrcode"></i>
+                </a>
+            </div>
+            <div class="col-1 d-flex justify-content-center">
+                <a href="#">
+                    <i class="fa-solid fa-pencil"></i>
+                </a>
+                <div class="update-profileimage">
+                    <i class="fa-solid fa-camera"></i>
+                </div>
             </div>
         @endif
+        @if(!request()->routeIs('settings'))
+            <div class="col-1 d-flex justify-content-center">
+                <a href="#" class="d-flex align-items-center">
+                    <i class="fa-solid fa-qrcode"></i>
+                </a>
+            </div>
+        @endif
+        <div class="col-1 d-flex justify-content-center" onclick="$('#burger-menu').toggle()">
+            <div class="burger-menu-toggler" style="cursor: pointer;padding:.1rem .5rem;">
+                <div class="burger"></div>
+                <div class="burger"></div>
+                <div class="burger"></div>
+            </div>
+        </div>
     </div>
 
     <div id="burger-menu" class="down">
