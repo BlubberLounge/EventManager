3adf7eca4ae54b5a3194722867163d41b3fd1876
Maximilian Mewes <mewes.maximilian@gmx.de>
2023-06-10 22:18:57 +0200

    Add `darkaonline/l5-swagger` ✨

--- app/Http/Controllers/Api/v1/Controller.php
+++ app/Http/Controllers/Api/v1/Controller.php
@@ -6,9 +6,30 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="EventManager REST Api",
+ *      description="BlubberLounge EventManager REST Api",
+ *      @OA\Contact(
+ *          email="admin@blubber-lounge.de"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Current Server"
+ * )
+ *
+ *
+ * @OA\Tag(
+ *     name="Users",
+ *     description="User Endpoints"
+ * )
+ */
 class Controller extends BaseController
 {
-    // use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests;
 
     /**
      * Create a new controller instance.
