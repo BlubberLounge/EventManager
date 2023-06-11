c98ac02e337d8b3391502787d38169dae281dfa4
Maximilian Mewes <45098772+itCarl@users.noreply.github.com>
2023-06-11 17:37:05 +0200

    #BLEM-31 Initial Laravel API setup (#49)
    * Add Api BaseController
    
    * Add Api AuthController with login and logout routes
    
    * Add Api versioning
    
    * User Model fix
    
    * Add `darkaonline/l5-swagger` ✨
    
    * Update Readme
    
    * Add Utillity Endpoint

---
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Api\v1\Controller;
+use App\Models\User;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    /**
+     * @OA\Get(
+     *      path="/user",
+     *      operationId="getUser",
+     *      tags={"Users"},
+     *      summary="Get current User",
+     *      description="Get current User",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
+    public function index()
+    {
+        return response()->json([
+            'name' => 'Abigail',
+            'state' => 'CA',
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
---
+++ routes/api_v1.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\Api\v1\UserController;
+use App\Http\Controllers\Api\v1\UtillityController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/ping', [UtillityController::class, 'ping']);
+
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function ()
+{
+    // Route::get('/u', function (Request $request) {
+    //     return $request->user();
+    // });
+
+    // Route::apiResource('user', UserController::class);
+});
